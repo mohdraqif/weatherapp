2f566df1b5f8e2e2c3a2f0161257fb20d32a27c2
mohdraqif <mohdraqif94@gmail.com>
2019-06-24 05:32:44 +0530

    Init commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/weather', (req, res) =>{                                    //////////
 app.post('/weather', (req, res) =>{
   var loc = req.body.location
   var location = loc.toUpperCase();
-    fetch('/weather?address='+ location).then((response) =>{
+    fetch('https://project-weatherapp.herokuapp.com/weather?address='+ location).then((response) =>{
       response.json().then((data) =>{
         // console.log(data.longitude,data.latitude,data.location)
          
